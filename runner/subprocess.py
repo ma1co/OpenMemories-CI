06171d8f2ebc042131ca49afc4ed5787339cc58d
ma1co <ma1co@users.noreply.github.com>
2021-01-11 20:50:08 +0000

    Extract Pipe class

--- runner/subprocess.py
+++ runner/subprocess.py
@@ -5,20 +5,64 @@ import sys
 import threading
 
 class SubprocessRunner:
- def __init__(self, name, args, cwd=None, timeout=10):
+ def __init__(self, name, args, cwd=None, timeout=10, log=True):
   self.p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, cwd=cwd, universal_newlines=True)
-  self.q = queue.Queue()
-  self.log = logging.getLogger(name)
+  self.stdio = Pipe(self.p.stdout, self.p.stdin, logging.getLogger(name + '.stdio') if log else None, timeout)
+  self.defaultPipe = self.stdio
+
+ def close(self):
+  self.stdio.close()
+
+ def wait(self):
+  self.p.wait()
+  self.close()
+
+ def finish(self):
+  self.p.terminate()
+  self.wait()
+
+ def __enter__(self):
+  return self
+
+ def __exit__(self, type, value, traceback):
+  self.finish()
+
+ def readLine(self):
+  return self.defaultPipe.readLine()
+
+ def expectLine(self, f):
+  return self.defaultPipe.expectLine(f)
+
+ def writeLine(self, data):
+  self.defaultPipe.writeLine(data)
+
+
+class PythonRunner(SubprocessRunner):
+ def __init__(self, script, args=[], timeout=10, log=True):
+  super().__init__(name=script, args=[sys.executable, '-u', '-m', script]+args, timeout=timeout, log=log)
+
+
+class Pipe:
+ def __init__(self, readFile, writeFile, log=None, timeout=10):
+  self.readFile = readFile
+  self.writeFile = writeFile
+  self.log = log
   self.timeout = timeout
+  self.q = queue.Queue()
   threading.Thread(target=self._asyncRead, daemon=True).start()
+
+ def close(self):
+  self.readFile.close()
+  self.writeFile.close()
 
  def _asyncRead(self):
   while True:
-   l = self.p.stdout.readline()
+   l = self.readFile.readline()
    self.q.put(l)
    if l == '':
     break
-   self.log.debug(l.rstrip('\n'))
+   if self.log:
+    self.log.debug(l.rstrip('\n'))
 
  def readLine(self):
   try:
@@ -33,22 +77,5 @@ class SubprocessRunner:
   return next(l for l in iter(self.readLine, None) if f(l))
 
  def writeLine(self, data):
-  self.p.stdin.write(data + '\n')
-  self.p.stdin.flush()
-
- def finish(self):
-  self.p.terminate()
-  self.p.wait(self.timeout)
-  self.p.stdin.close()
-  self.p.stdout.close()
-
- def __enter__(self):
-  return self
-
- def __exit__(self, type, value, traceback):
-  self.finish()
-
-
-class PythonRunner(SubprocessRunner):
- def __init__(self, script, args=[], timeout=10):
-  super().__init__(name=script, args=[sys.executable, '-u', '-m', script]+args, timeout=timeout)
+  self.writeFile.write(data + '\n')
+  self.writeFile.flush()
