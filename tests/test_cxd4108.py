e438ca9a5d8d14ef1e73a4f194a34194118640e5
ma1co <ma1co@users.noreply.github.com>
2021-04-01 15:12:23 +0000

    Refactor CXD4108 tests

--- tests/test_cxd4108.py
+++ tests/test_cxd4108.py
@@ -1,3 +1,4 @@
+import glob
 import os
 from PIL import Image, ImageChops
 import textwrap
@@ -7,54 +8,110 @@ import unittest
 from . import TestCase
 from runner import archive, kernel_patch, onenand, qemu, zimage
 
+
+class FirmwareDump:
+ def __init__(self, dir):
+  self._dir = dir
+
+ def _readFile(self, name, dir=None):
+  with open(os.path.join(dir or self._dir, name), 'rb') as f:
+   return f.read()
+
+ def getBootRom(self):
+  return self._readFile('bootrom')
+
+ def getBootPartition(self):
+  return self._readFile('boot')
+
+ def getPartition(self, i):
+  if i not in [1, 2, 3, 5, 6]:
+   raise Exception('Invalid partition')
+  return archive.readFat(self._readFile('nflasha%d' % i))
+
+
+class FirmwareUpdate(FirmwareDump):
+ def __init__(self, updateDir, dumpDir):
+  super().__init__(dumpDir)
+  self._updateDir = updateDir
+
+ def _readUpdateFile(self, name):
+  return self._readFile(name, self._updateDir)
+
+ def _readUpdateTar(self, name):
+  p = archive.Archive()
+  for fn in glob.iglob(os.path.join(self._updateDir, name)):
+   with open(fn, 'rb') as f:
+    p.writeAll(archive.readTar(f.read()))
+  return p
+
+ def getPartition(self, i):
+  p = archive.Archive()
+
+  if i == 2:
+   for f in ['backup.tar', 'factory.tar']:
+    p.writeAll(self._readUpdateTar(f))
+   for f in ['Asys', 'Hsys']:
+    p.write('/factory/%s.bin' % f, self._readFile('%s.bin' % f))
+    p.write('/factory/%s2.bak' % f, self._readFile('%s.bin' % f))
+   p.patch('/factory/Areg.bin', lambda d: p.read('/factory/Asys.bin')[:1] + d[1:])
+
+  elif i == 3:
+   p.writeAll(self._readUpdateTar('linuxset[1-9].tar'), '/boot')
+
+  elif i == 5:
+   for f in ['av.bin', 'sa.bin']:
+    p.write('/%s' % f, self._readUpdateFile(f))
+
+  elif i == 6:
+   for f in ['bin.tar', 'fskapp[1-9].tar', 'fskfnt.tar', 'fskrel[1-9].tar', 'lib.tar']:
+    p.writeAll(self._readUpdateTar(f))
+
+  else:
+   p = super().getPartition(i)
+
+  return p
+
+
 class TestCXD4108(TestCase):
  MACHINE = 'cxd4108'
  NAND_SIZE = 0x4000000
 
- MODEL = 'DSC-W90'
- FIRMWARE_DIR = 'firmware/DSC-W90'
- SCREENSHOT_DIR = 'screenshots/DSC-W90'
-
- def readFirmwareFile(self, name):
-  with open(os.path.join(self.FIRMWARE_DIR, name), 'rb') as f:
-   return f.read()
-
  def prepareBootRom(self):
-  return self.readFirmwareFile('bootrom')
+  return self.firmware.getBootRom()
 
  def prepareBootPartition(self):
-  return self.readFirmwareFile('boot')
+  return self.firmware.getBootPartition()
 
  def prepareUpdaterKernel(self, unpackZimage=False):
-  kernel = archive.readFat(self.readFirmwareFile('nflasha1')).read('/boot/vmlinux')
+  kernel = self.firmware.getPartition(1).read('/boot/vmlinux')
   if unpackZimage:
    kernel = zimage.unpackZimage(kernel)
   return kernel
 
  def prepareUpdaterInitrd(self, shellOnly=False):
-  initrd = archive.readCramfs(archive.readFat(self.readFirmwareFile('nflasha1')).read('/boot/initrd.img'))
+  initrd = archive.readCramfs(self.firmware.getPartition(1).read('/boot/initrd.img'))
   if shellOnly:
    initrd.write('/sbin/init', b'#!/bin/sh\nmount -t proc proc /proc\nwhile true; do sh; done\n')
   return archive.writeCramfs(initrd)
 
  def prepareMainKernel(self, patchConsoleEnable=False):
-  kernel = archive.readFat(self.readFirmwareFile('nflasha3')).read('/boot/vmlinux')
+  kernel = self.firmware.getPartition(3).read('/boot/vmlinux')
   if patchConsoleEnable:
    kernel = kernel_patch.patchConsoleEnable(kernel)
   return kernel
 
  def prepareFlash1(self, kernel=None, initrd=None):
-  nflasha1 = archive.readFat(self.readFirmwareFile('nflasha1'))
+  nflasha1 = self.firmware.getPartition(1)
   if kernel:
    nflasha1.write('/boot/vmlinux', kernel)
   if initrd:
    nflasha1.write('/boot/initrd.img', initrd)
   return archive.writeFat(nflasha1, 0x200000)
 
- def prepareFlash2(self, readSettings=False, updaterMode=False, playbackMode=False):
+ def prepareFlash2(self, readSettings=False, updaterMode=False, playbackMode=False, patchTouchscreenEnable=False):
   nflasha2 = archive.Archive()
   if readSettings:
-   settings = archive.readFat(self.readFirmwareFile('nflasha2'))
+   settings = self.firmware.getPartition(2)
    for fn in settings.files:
     if fn.startswith('/backup/') or fn.startswith('/factory/'):
      nflasha2.write(fn, settings.read(fn))
@@ -62,10 +119,12 @@ class TestCXD4108(TestCase):
    nflasha2.write('/updater/mode', b'')
   if playbackMode:
    nflasha2.patch('/factory/Areg.bin', lambda d: d[:0x100] + b'\x84' + d[0x101:])
+  if patchTouchscreenEnable:
+   nflasha2.patch('/factory/Asys.bin', lambda d: d[:0x2a5] + b'\x01' + d[0x2a6:])
   return archive.writeFat(nflasha2, 0x180000)
 
  def prepareFlash3(self, kernel=None, rootfs=None):
-  nflasha3 = archive.readFat(self.readFirmwareFile('nflasha3'))
+  nflasha3 = self.firmware.getPartition(3)
   if kernel:
    nflasha3.write('/boot/vmlinux', kernel)
   if rootfs:
@@ -73,19 +132,43 @@ class TestCXD4108(TestCase):
   return archive.writeFat(nflasha3, 0x400000)
 
  def prepareFlash5(self):
-  return self.readFirmwareFile('nflasha5')
+  nflasha5 = self.firmware.getPartition(5)
+  return archive.writeFat(nflasha5, 0x380000)
 
  def prepareFlash6(self):
-  return self.readFirmwareFile('nflasha6')
+  nflasha6 = self.firmware.getPartition(6)
+  return archive.writeFat(nflasha6, 0x1000000)
 
  def prepareFlash11(self):
-  return archive.writeMbr([archive.writeFat(archive.Archive(), 0x100000)])
+  nflasha11 = archive.Archive()
+  return archive.writeMbr([archive.writeFat(nflasha11, 0x7ffe00)])
 
  def prepareNand(self, boot=b'', partitions=[]):
   return onenand.writeNand(boot, archive.writeFlash(partitions), self.NAND_SIZE, 0x100000)
 
- def prepareQemuArgs(self, bootRom=None, kernel=None, initrd=None, nand=None):
+ def prepareQemuArgs(self, bootRom=None, kernel=None, initrd=None, nand=None, mmc=None):
   args = ['-icount', 'shift=4']
+  if bootRom:
+   args += ['-bios', bootRom]
+  if kernel:
+   args += ['-kernel', kernel]
+  if initrd:
+   args += ['-initrd', initrd]
+  if nand:
+   args += ['-drive', 'file=%s,if=mtd,format=raw' % nand]
+   if mmc:
+    args += ['-drive', 'file=%s,if=mtd,format=raw' % mmc]
+  return args
+
+
+class TestDscW90(TestCXD4108):
+ FIRMWARE_DIR = 'firmware/DSC-W90'
+ SCREENSHOT_DIR = 'screenshots/DSC-W90'
+
+ firmware = FirmwareDump(FIRMWARE_DIR)
+
+ def prepareQemuArgs(self, bootRom=None, kernel=None, initrd=None, nand=None):
+  args = super().prepareQemuArgs(bootRom=bootRom, kernel=kernel, initrd=initrd, nand=nand)
 
   # Power IC
   args += ['-device', 'bionz_mb89083,id=mb89083,bus=/sio0', '-connect-gpio', 'odev=gpio1,onum=1,idev=mb89083,iname=ssi-gpio-cs']
@@ -96,14 +179,6 @@ class TestCXD4108(TestCase):
   # Buttons
   args += ['-device', 'bionz_buttons,bus=/adc0,keys0=druls,keys1=wtmh']
 
-  if bootRom:
-   args += ['-bios', bootRom]
-  if kernel:
-   args += ['-kernel', kernel]
-  if initrd:
-   args += ['-initrd', initrd]
-  if nand:
-   args += ['-drive', 'file=%s,if=mtd,format=raw' % nand]
   return args
 
  def checkShell(self, func, checkCpuinfo=True, checkVersion=True):
@@ -238,78 +313,15 @@ class TestCXD4108(TestCase):
 
 
 
-class TestDscG3(TestCase):
- MACHINE = 'cxd4108'
- NAND_SIZE = 0x4000000
-
+class TestDscG3(TestCXD4108):
  FIRMWARE_DIR = 'firmware/DSC-G3'
  FIRMWARE_DUMP_DIR = 'firmware/DSC-W90'
  SCREENSHOT_DIR = 'screenshots/DSC-G3'
 
- def readFirmwareFile(self, name):
-  with open(os.path.join(self.FIRMWARE_DIR, name), 'rb') as f:
-   return f.read()
-
- def readFirmwareDumpFile(self, name):
-  with open(os.path.join(self.FIRMWARE_DUMP_DIR, name), 'rb') as f:
-   return f.read()
-
- def prepareBootRom(self):
-  return self.readFirmwareDumpFile('bootrom')
-
- def prepareBootPartition(self):
-  return self.readFirmwareDumpFile('boot')
-
- def prepareMainKernel(self, patchConsoleEnable=False):
-  kernel = archive.readTar(self.readFirmwareFile('linuxset1.tar')).read('/vmlinux')
-  if patchConsoleEnable:
-   kernel = kernel_patch.patchConsoleEnable(kernel)
-  return kernel
-
- def prepareFlash2(self, patchTouchscreenEnable=False, patchAregVersion=False, playbackMode=False):
-  nflasha2 = archive.Archive()
-  for f in ['backup.tar', 'factory.tar']:
-   nflasha2.writeAll(archive.readTar(self.readFirmwareFile(f)))
-  for f in ['Asys', 'Hsys']:
-   nflasha2.write('/factory/%s.bin' % f, self.readFirmwareDumpFile('%s.bin' % f))
-   nflasha2.write('/factory/%s2.bak' % f, self.readFirmwareDumpFile('%s.bin' % f))
-  if patchTouchscreenEnable:
-   nflasha2.patch('/factory/Asys.bin', lambda d: d[:0x2a5] + b'\x01' + d[0x2a6:])
-  if patchAregVersion:
-   nflasha2.patch('/factory/Areg.bin', lambda d: b'\x04' + d[1:])
-  if playbackMode:
-   nflasha2.patch('/factory/Areg.bin', lambda d: d[:0x100] + b'\x84' + d[0x101:])
-  return archive.writeFat(nflasha2, 0x180000)
-
- def prepareFlash3(self, kernel=None, rootfs=None):
-  nflasha3 = archive.Archive()
-  nflasha3.writeAll(archive.readTar(self.readFirmwareFile('linuxset1.tar')), '/boot')
-  if kernel:
-   nflasha3.write('/boot/vmlinux', kernel)
-  if rootfs:
-   nflasha3.write('/boot/rootfs.img', rootfs)
-  return archive.writeFat(nflasha3, 0x400000)
-
- def prepareFlash5(self):
-  nflasha5 = archive.Archive()
-  for f in ['av.bin', 'sa.bin']:
-   nflasha5.write('/%s' % f, self.readFirmwareFile(f))
-  return archive.writeFat(nflasha5, 0x380000)
-
- def prepareFlash6(self):
-  nflasha6 = archive.Archive()
-  for f in ['bin.tar', 'fskapp1.tar', 'fskfnt.tar', 'fskrel1.tar', 'fskrel2.tar', 'lib.tar']:
-   nflasha6.writeAll(archive.readTar(self.readFirmwareFile(f)))
-  return archive.writeFat(nflasha6, 0x1de0000)
-
- def prepareMmc(self):
-  return archive.writeMbr([archive.writeFat(archive.Archive(), 0x7ffe00)])
-
- def prepareNand(self, boot=b'', partitions=[]):
-  return onenand.writeNand(boot, archive.writeFlash(partitions), self.NAND_SIZE, 0x100000)
+ firmware = FirmwareUpdate(FIRMWARE_DIR, FIRMWARE_DUMP_DIR)
 
  def prepareQemuArgs(self, bootRom=None, nand=None, mmc=None):
-  args = ['-icount', 'shift=4']
+  args = super().prepareQemuArgs(bootRom=bootRom, nand=nand, mmc=mmc)
 
   # Power IC
   args += ['-device', 'bionz_mb89083,id=mb89083,bus=/sio0', '-connect-gpio', 'odev=gpio1,onum=1,idev=mb89083,iname=ssi-gpio-cs']
@@ -324,12 +336,6 @@ class TestDscG3(TestCase):
   # Touch panel
   args += ['-device', 'bionz_touch_panel,id=touch_panel,bus=/adc0', '-connect-gpio', 'odev=gpio3,onum=5,idev=touch_panel,inum=0', '-connect-gpio', 'odev=gpio3,onum=6,idev=touch_panel,inum=1']
 
-  if bootRom:
-   args += ['-bios', bootRom]
-  if nand:
-   args += ['-drive', 'file=%s,if=mtd,format=raw' % nand]
-   if mmc:
-    args += ['-drive', 'file=%s,if=mtd,format=raw' % mmc]
   return args
 
 
@@ -340,14 +346,14 @@ class TestDscG3(TestCase):
     boot=self.prepareBootPartition(),
     partitions=[
      b'',
-     self.prepareFlash2(patchTouchscreenEnable=True, patchAregVersion=True, playbackMode=True),
+     self.prepareFlash2(readSettings=True, patchTouchscreenEnable=True, playbackMode=True),
      self.prepareFlash3(kernel=self.prepareMainKernel(patchConsoleEnable=True)),
      b'',
      self.prepareFlash5(),
      self.prepareFlash6(),
     ],
    ),
-   'mmc.dat': self.prepareMmc(),
+   'mmc.dat': self.prepareFlash11(),
   }
   args = self.prepareQemuArgs(bootRom='rom.dat', nand='nand.dat', mmc='mmc.dat')
 
